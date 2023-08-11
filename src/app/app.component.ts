9ba1d29bbcaf669f76f17817bf021b27adf467b5
Efe Mert Yalcin <efemertyalcin@gmail.com>
2023-08-11 13:39:13 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherAPP';
+export class AppComponent implements OnInit {
+
+  cityName: string = 'Istanbul'; 
+  currentTemperature: number = 0;
+  minTemperature: number = 0;
+  maxTemperature: number = 0;
+  humidity: number = 0;
+  windSpeed: number = 0;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData(53.1, -0.13) 
+      .subscribe(
+        (data: any) => {
+          this.currentTemperature = data.current.temp_c;
+          this.minTemperature = data.current.feelslike_c;
+          this.maxTemperature = data.current.feelslike_c;
+          this.humidity = data.current.humidity;
+          this.windSpeed = data.current.wind_kph;
+        },
+        (error: any) => {
+          console.error(error);
+        }
+      );
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,14 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+  private apiKey = 'c153d8a4e2msh4c8a8de7f4447e1p1cf5a9jsn3c304999c42b'; 
+  
   constructor(private http: HttpClient) { }
 
-  GetWeatherData(cityName: string) {
-    this.http.get()
+  getWeatherData(latitude: number, longitude: number): Observable<any> {
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${latitude}%2C${longitude}`;
+    
+    const headers = new HttpHeaders({
+      'X-RapidAPI-Key': this.apiKey,
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    });
+
+    return this.http.get(url, { headers });
   }
 }
